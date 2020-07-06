bb15eca91fca3ec2d0e903632f59bc0ffaa36970
nightpablo <pablo_barragan@hotmail.com>
2020-07-06 19:38:21 -0300

    docs agregado

---
+++ docs/precache-manifest.976056d065a0a34d3d56f6a736a72958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43178b80c3d08d0d329a1fb023db90c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a035cd7ed01e725c70c",
+    "url": "/static/css/main.e13a6c2e.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a035cd7ed01e725c70c",
+    "url": "/static/js/main.c3c31ca6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
